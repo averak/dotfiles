f9f3c6e4bebcdccb55f2ac91bc609e1ed0a5ee11
AjxLab <abe12@mccc.jp>
2020-01-15 14:36:41 +0900

    installer for mac

--- macOS/install.sh
+++ macOS/install.sh
@@ -15,6 +15,47 @@ if [ ! -e ~/.pyenv ];then
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 fi
 
+# install enable-shared python using pyenv
+if [ ! -e "${HOME}"/.pyenv/versions/3.7.4 ];then
+    ~/.pyenv/bin/pyenv install 3.7.4
+    ~/.pyenv/bin/pyenv global 3.7.4
+else
+    echo "Python 3.7.4 is already installed."
+fi
+
+# install rbenv
+if [ ! -e ~/.rbenv ];then
+    echo "Installing rbenv..."
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+    echo "Installing ruby-build..."
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+
+# install enable-shared ruby using rbenv
+if [ ! -e "${HOME}"/.rbyenv/versions/2.7.0 ];then
+    ~/.rbenv/bin/rbenv install 2.7.0
+    ~/.rbenv/bin/rbenv global 2.7.0
+else
+    echo "Ruby 2.7.0 is already installed."
+fi
+
+# install nodenv
+if [ ! -e ~/.nodenv ];then
+    echo "Installing nodenv..."
+    git clone git://github.com/nodenv/nodenv.git ~/.nodenv
+fi
+
+# install nodejs
+if [ ! -e "${HOME}"/.nodenv/versions/13.6.0 ];then
+    ~/.nodenv/bin/nodenv install 13.6.0
+    ~/.nodenv/bin/nodenv global 13.6.0
+else
+    echo "Nodejs 13.6.0 is already installed."
+fi
+
+# .gitconfig
+cp .gitconfig ~/
+
 # install fzf
 if [ ! -e ~/.fzf ];then
     echo "Installing fzf..."
@@ -22,17 +63,6 @@ if [ ! -e ~/.fzf ];then
     workdir=${PWD}
     cd ~/.fzf && ./install --key-bindings --no-completion --no-update-rc && cd ${workdir}
 fi
-
-# install enable-shared python using pyenv
-if [ ! -e "${HOME}"/.pyenv/versions/3.7.4 ];then
-    CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.4
-else
-    echo "Python 3.7.4 is already installed."
-fi
-
-# set python
-pyenv shell --unset
-pyenv global 3.7.4
 
 # install vim
 ROOTDIR=$PWD
