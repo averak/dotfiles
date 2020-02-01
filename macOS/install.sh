b03ca8a64daf929b4e50e6005b65ad2c09b98d5c
AjxLab <abe12@mccc.jp>
2020-02-02 02:57:46 +0900

    不要なパッケージのインストールを解除

--- macOS/install.sh
+++ macOS/install.sh
@@ -5,4 +5,4 @@ if [ ! -e /usr/local/bin/brew ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install git wget curl openssl krb5 re2c bison libxml2 autoconf automake icu4c libjpeg libpng libmcrypt
+brew install git openssllibxml2 autoconf automake
