bf2e9e138e063a3b581925edb12c11f922c0b04d
averak <averak.jp@gmail.com>
2024-12-30 07:22:48 +0900

    show tabline

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -4,11 +4,38 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             require("lualine").setup({
-                options = { theme = "powerline_dark" },
+                options = { theme = "material" },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch' },
+                    lualine_c = {
+                        {
+                            'filename',
+                            path = 1
+                        }
+                    },
+                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' }
+                },
+                tabline = {
+                    lualine_a = { 'buffers' },
+                    lualine_b = {
+                        {
+                            'tabs',
+                            mode = 2
+                        }
+                    },
+                    lualine_c = {
+                        {
+                            function()
+                                -- 現在のバッファのファイルパスを表示
+                                return vim.fn.expand('%:~:.')
+                            end
+                        }
+                    }
+                },
             })
         end,
-    },
-    {
-        "Bekaboo/dropbar.nvim",
     },
 }
