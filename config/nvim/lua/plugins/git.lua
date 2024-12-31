c87ef48a881715500918b9ae9075202413931fb1
averak <averak.jp@gmail.com>
2024-12-31 23:35:45 +0900

    git操作のキーバインドを整理した

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -3,6 +3,22 @@ return {
         "lewis6991/gitsigns.nvim",
         lazy = true,
         event = { "BufReadPost", "BufNewFile" },
+        keys = {
+            {
+                "<leader>gu",
+                function()
+                    require("gitsigns").preview_hunk()
+                end,
+                desc = "Git diff",
+            },
+            {
+                "<leader>ge",
+                function()
+                    require("gitsigns").blame_line()
+                end,
+                desc = "Git blame",
+            }
+        },
         config = true,
     },
     {
